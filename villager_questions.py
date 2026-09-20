df67e6006a1445dcd8a5c19d80d2945be1c5d496
Dr4zza <drfzaa@gmail.com>
2026-09-20 16:27:18 +0530

    bug fix

--- villager_questions.py
+++ villager_questions.py
@@ -1,10 +1,8 @@
 import random
-questions = ['\nWhat happened during the night? Slept well?', '\nWhat nightmare did you have ? ',
-             '\nWho are you so sure that you arent dead yet ?', '\nWho did you kill tonight?', 'Who do you plan to kill tomorrow?']
+questions = ['\nWhat happened during the night? Slept well?', '\nWhat nightmare did you have ?',
+             "\nWho are you so sure that you is'nt dead yet ?", '\nWho did you kill tonight?', '\nWho do you plan to kill tomorrow?']
 
-# Replace 'self' with 'broadcast_func'
 def start_question_round(broadcast_func):
     q = random.choice(questions)
-    # Call the function directly instead of using .broadcast()
     broadcast_func("\n ====== VILLAGER QUESTION ROUND ======")
     broadcast_func(f"{q}\n")
